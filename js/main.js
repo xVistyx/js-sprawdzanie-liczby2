b8690d2d95d26701319494c3e944b3865a0b2537
xVistyx <wiktor.hc@gmail.com>
2022-07-18 13:35:35 +0200

    js-sprawdzanie-liczby2

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let liczba1 = 20;
+let liczba2 = 30;
+
+function przedzial (_par1, _par2) {
+
+if (_par1 === 20) {
+    if (_par2 === 20) {
+        if (_par1 + _par2 >= 20) {
+            return "true"
+        }
+    }
+}
+else {
+    return "false"
+}
+
+}
+console.log(przedzial(liczba1, liczba2));
